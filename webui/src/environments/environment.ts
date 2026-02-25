38629dd712a08d7d066c1c0b66abef68371ec42a
lgthvinh <everwellmax@gmail.com>
2026-02-25 22:51:45 +0700

    feat: update apiBaseUrl to use localhost for local development

--- webui/src/environments/environment.ts
+++ webui/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   useFakeData: false,
-  apiBaseUrl: '' // Use relative path to support both localhost and 127.0.0.1
+  apiBaseUrl: 'http://localhost' // Use relative path to support both localhost and 127.0.0.1
 };
