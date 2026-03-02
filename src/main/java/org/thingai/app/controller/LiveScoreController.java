2449f34f4b19a0165c54f8d2af318699cf894316
lgthevinh <everwellmax@gmai.com>
2026-03-02 06:05:08 -0500

    feat: refactor live score update handling and remove unused sync endpoints
    Signed-off-by: lgthevinh <everwellmax@gmai.com>

--- src/main/java/org/thingai/app/controller/LiveScoreController.java
+++ src/main/java/org/thingai/app/controller/LiveScoreController.java
@@ -3,25 +3,16 @@ package org.thingai.app.controller;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Controller;
-import org.thingai.app.scoringservice.ScoringService;
-import org.thingai.app.scoringservice.dto.LiveScoreUpdateDto;
-import org.thingai.base.log.ILog;
 
 @Controller
 public class LiveScoreController {
 
-    @MessageMapping("/live/score/update/blue")
-    public void handleLiveScoreUpdate(@Payload LiveScoreUpdateDto updateDto) {
-        ILog.d("Received live score update of BLUE alliance", updateDto.payload.at);
-
-        ScoringService.liveScoreHandler().handleLiveScoreUpdate(updateDto, false);
+    @MessageMapping("live/score/blue")
+    public void handleLiveScoreUpdateBlue(@Payload String jsonUpdate) {
     }
 
-    @MessageMapping("/live/score/update/red")
-    public void handleLiveScoreUpdateRed(@Payload LiveScoreUpdateDto updateDto) {
-        ILog.d("Received live score update of RED alliance ", updateDto.payload.at);
-
-        ScoringService.liveScoreHandler().handleLiveScoreUpdate(updateDto, true);
+    @MessageMapping("live/score/red")
+    public void handleLiveScoreUpdateRed(@Payload String jsonUpdate) {
     }
 
 }
