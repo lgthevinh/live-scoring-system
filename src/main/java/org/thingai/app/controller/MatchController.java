ede86c0a37819cff67c75386e44c051289ff9cdc
lgthevinh <everwellmax@gmai.com>
2026-03-02 04:45:41 -0500

    refactor: remove distraction code
    Signed-off-by: lgthevinh <everwellmax@gmai.com>

--- src/main/java/org/thingai/app/controller/MatchController.java
+++ src/main/java/org/thingai/app/controller/MatchController.java
@@ -7,8 +7,6 @@ import org.thingai.app.scoringservice.entity.match.Match;
 
 import java.util.Map;
 
-import static org.thingai.app.controller.utils.ResponseEntityUtil.getObjectResponse;
-
 @RestController
 @RequestMapping("/api/match")
 public class MatchController {
@@ -17,48 +15,47 @@ public class MatchController {
 
     @PostMapping("/create")
     public ResponseEntity<Object> createMatch(@RequestBody Map<String, Object> request) {
-
+        return null;
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Object> getMatch(@PathVariable String id) {
-
+        return null;
     }
 
     @GetMapping("")
     public ResponseEntity<Object> listMatches() {
-
+        return null;
     }
 
     @GetMapping("/list/{matchType}")
     public ResponseEntity<Object> listMatchesByType(@PathVariable int matchType) {
-
+        return null;
     }
 
     @GetMapping("/list/details/{matchType}")
     public ResponseEntity<Object> listMatchDetails(@PathVariable int matchType, @RequestParam(required = false) boolean withScore) {
-
+        return null;
     }
 
     @PutMapping("/update")
     public ResponseEntity<Object> updateMatch(@RequestBody Match match) {
-
+        return null;
     }
 
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<Object> deleteMatch(@PathVariable String id) {
-
+        return null;
     }
 
     @PostMapping("/schedule/generate")
     public ResponseEntity<Object> generateSchedule(@RequestBody Map<String, Object> request) {
-
-        return getObjectResponse(future);
+        return null;
     }
 
     @PostMapping("/playoff/generate")
     public ResponseEntity<Object> generatePlayoffSchedule(@RequestBody Map<String, Object> request) {
-
+        return null;
     }
 }
 
