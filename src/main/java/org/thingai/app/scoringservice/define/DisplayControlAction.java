c3c56491bf6f0b2d60d4e082a705dc01c4454222
lgthevinh <everwellmax@gmai.com>
2026-03-06 04:43:00 -0500

    feat: add constants for displaying results and rankings in DisplayControlAction
    Signed-off-by: lgthevinh <everwellmax@gmai.com>

--- src/main/java/org/thingai/app/scoringservice/define/DisplayControlAction.java
+++ src/main/java/org/thingai/app/scoringservice/define/DisplayControlAction.java
@@ -4,5 +4,6 @@ public class DisplayControlAction {
     public static final int SHOW_BLANK = 0;
     public static final int SHOW_MATCH = 1;
     public static final int SHOW_PREVIEW = 2;
-
+    public static final int SHOW_RESULT = 3;
+    public static final int SHOW_RANKING = 4;
 }
