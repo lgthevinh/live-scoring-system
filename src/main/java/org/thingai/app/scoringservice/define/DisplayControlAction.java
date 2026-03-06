3973e510c22d09fd45716024c5b89bcc5896ccde
lgthevinh <everwellmax@gmai.com>
2026-03-06 04:48:47 -0500

    feat: add ranking event topic and update constants for score and ranking in DisplayControlAction
    Signed-off-by: lgthevinh <everwellmax@gmai.com>

--- src/main/java/org/thingai/app/scoringservice/define/DisplayControlAction.java
+++ src/main/java/org/thingai/app/scoringservice/define/DisplayControlAction.java
@@ -6,4 +6,8 @@ public class DisplayControlAction {
     public static final int SHOW_PREVIEW = 2;
     public static final int SHOW_RESULT = 3;
     public static final int SHOW_RANKING = 4;
+
+    public static final int UPDATE_SCORE = 256;
+    public static final int UPDATE_RANKING = 257;
+    public static final int UPDATE_MATCH = 258;
 }
