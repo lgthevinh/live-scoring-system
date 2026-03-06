7b933b2cdde7acfec38729323eff195bb796d490
lgthevinh <everwellmax@gmai.com>
2026-03-06 05:06:04 -0500

    feat: refactor team management by introducing TeamHandler for handling team operations
    Signed-off-by: lgthevinh <everwellmax@gmai.com>

--- src/main/java/org/thingai/app/controller/utils/ResponseEntityUtil.java
+++ src/main/java/org/thingai/app/controller/utils/ResponseEntityUtil.java
@@ -22,9 +22,9 @@ public class ResponseEntityUtil {
     public static synchronized ResponseEntity<Object> createErrorResponse(int errorCode, String errorMessage) {
         Map<String, Object> body = Map.of("errorCode", errorCode, "error", errorMessage);
         HttpStatus status = switch (errorCode) {
-            case ErrorCode.CREATE_FAILED,
-                 ErrorCode.UPDATE_FAILED,
-                 ErrorCode.DELETE_FAILED -> HttpStatus.BAD_REQUEST;
+            case ErrorCode.DAO_CREATE_FAILED,
+                 ErrorCode.DAO_UPDATE_FAILED,
+                 ErrorCode.DAO_DELETE_FAILED -> HttpStatus.BAD_REQUEST;
             case ErrorCode.NOT_FOUND -> HttpStatus.NOT_FOUND;
             default -> HttpStatus.INTERNAL_SERVER_ERROR;
         };
